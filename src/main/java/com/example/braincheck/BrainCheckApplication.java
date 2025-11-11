a38976cc77861879a1e4ea6634a05a057f6d7cdf
minju <sfhketum1231@gmail.com>
2025-11-11 16:28:34 +0900

    Initial commit

---
+++ src/main/java/com/example/braincheck/BrainCheckApplication.java
@@ -0,0 +1,13 @@
+package com.example.braincheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrainCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/braincheck/BrainCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.braincheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
