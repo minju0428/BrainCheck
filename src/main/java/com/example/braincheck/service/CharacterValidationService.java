0b7c3bdf06b9b26a0f455e946bfb7445108a3ca7
minju <sfhketum1231@gmail.com>
2025-11-16 21:08:44 +0900

    feat: 등장인물 AI 검증 서비스 초기 구조 생성

---
+++ src/main/java/com/example/braincheck/service/CharacterValidationService.java
@@ -0,0 +1,10 @@
+package com.example.braincheck.service;
+//CharacterController.java 파일에 지정된 변수들을 ai로 검증하는 코드 파일
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CharacterValidationService {
+    //추후 ai 연동 코드 적기
+
+}
