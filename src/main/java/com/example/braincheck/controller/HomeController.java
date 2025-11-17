59814e3a2ea1c5a08af00d470e283c52fee2bebd
minju <sfhketum1231@gmail.com>
2025-11-17 23:20:25 +0900

    feat: 루트 경로(/)에 메인 화면(HomeActivity.html) 연결
    초기 접속 시 표시될 HomeActivity.html을 static 리소스로 연결하고,
    HomeController를 통해 '/' 경로로 접근 시 자동 응답하도록 설정함

---
+++ src/main/java/com/example/braincheck/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.braincheck.controller;
+//스프링 부트3 실행 시 메인 화면 열리게 하는 코드
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        //forward :  : 서버 내부에  다른 경로로 전달하는 접두사
+        //해당 파일의 경로를 찾아줌.
+        return "HomeActivity.html";
+    }
+}
