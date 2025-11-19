abd88677ceb80c2632d9fa6b3538421c4f47df9f
minju <sfhketum1231@gmail.com>
2025-11-19 13:51:30 +0900

    refactor: CharacterForm.java 파일명을 CharacterFormDto.java로 변경
    - DTO 역할을 명확히 하기 위해 파일명 및 클래스명을 일치하도록 수정

--- src/main/java/com/example/braincheck/dto/CharacterFormDto.java
+++ src/main/java/com/example/braincheck/dto/CharacterFormDto.java
@@ -3,7 +3,7 @@ package com.example.braincheck.dto;
 
 import jakarta.validation.constraints.NotBlank;
 
-public class CharacterForm {
+public class CharacterFormDto {
     //필수항목
     @NotBlank(message = "등장인물 이름은 필수 입력 항목입니다.")
     private String characterName;
